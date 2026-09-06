f7690208cab9ed82d428bcc8b34ca2f45d0173a5
sahilshahane <sahilpshahane123@gmail.com>
2026-09-06 22:43:55 +0530

    feat: TRACES DIR env

--- run_batch.sh
+++ run_batch.sh
@@ -5,9 +5,11 @@ find util/job_launching/procman -type f  ! -name "*.py" -delete
 
 GPUS=("A100" "H100" "H200" "RTX2060")
 
+TRACES_DIR="./hw_run_teslav100/rodinia_2.0-ft/11.0"
+
 for GPU in "${GPUS[@]}"
 do
-    ./get_results.sh -g $GPU
+    ./get_results.sh -g $GPU -d $TRACES_DIR
 
     if [ $? -ne 0 ]; then
         echo "Simulation failed for $GPU"
